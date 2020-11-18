3581b2888e8b706445c1ec889d3bb2c97e74b1d3
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-11-18 20:09:36 +0100

    spring project

---
+++ src/main/java/com/ao/jenkins1ao/Jenkins1aoApplication.java
@@ -0,0 +1,13 @@
+package com.ao.jenkins1ao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins1aoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkins1aoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ao/jenkins1ao/Jenkins1aoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ao.jenkins1ao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins1aoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
